7014e5bb38210ef2d758aa5cbae3f3df8b4bedd7
unclecode <unclecode@kidocode.com>
2024-09-15 12:56:17 +0800

    Initial Commit

---
+++ WhispeAnywhere/WhispeAnywhereApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhispeAnywhereApp.swift
+//  WhispeAnywhere
+//
+//  Created by Unclecode on 15/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhispeAnywhereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
